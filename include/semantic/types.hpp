ed18ee3ba1e750f44f3ffcef8b079948428e70ee
d-u-d-e <mari8.marco@gmail.com>
2025-03-03 21:16:32 +0100

    feat: semantic types

---
+++ include/semantic/types.hpp
@@ -0,0 +1,62 @@
+#pragma once
+#include <memory>
+#include <optional>
+#include <symbol.hpp>
+#include <vector>
+
+namespace semantic
+{
+
+namespace types
+{
+
+class Type {
+  public:
+  virtual ~Type() = default;
+};
+
+struct Integer : public Type { };
+struct String : public Type { };
+struct Nil : public Type { };
+struct Unit : public Type {
+}; // Used to indicate that an expression returns no value
+
+struct Record : public Type {
+  Record(std::vector<std::pair<symbol::Symbol, std::shared_ptr<Type>>> fields,
+         uint32_t unique)
+    : fields(std::move(fields))
+    , unique(unique)
+  { }
+
+  bool operator==(const Record& other) const
+  {
+    return unique == other.unique;
+  }
+
+  std::vector<std::pair<symbol::Symbol, std::shared_ptr<Type>>> fields;
+  uint32_t unique;
+};
+
+struct Array : public Type {
+  Array(std::shared_ptr<Type> type, uint32_t unique)
+    : type(std::move(type))
+    , unique(unique)
+  { }
+
+  std::shared_ptr<Type> type;
+  uint32_t unique;
+};
+
+struct Name : public Type {
+  Name(symbol::Symbol name, std::shared_ptr<Type> type)
+    : name(name)
+    , type(std::move(type))
+  { }
+
+  symbol::Symbol name;
+  std::shared_ptr<Type> type;
+};
+
+} // namespace types
+
+}; // namespace semantic
