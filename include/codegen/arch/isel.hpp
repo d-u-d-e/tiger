c42f134f86be967aaae6aa233ff4335bba5e534d
d-u-d-e <mari8.marco@gmail.com>
2025-04-03 16:21:59 +0200

    chore: updated comment

--- include/codegen/arch/isel.hpp
+++ include/codegen/arch/isel.hpp
@@ -1,12 +1,19 @@
 #pragma once
 
 /* 
-The following grammar is definitely simple and incomplete for x86-64 ISA,
-but a good start to make instruction selection work. For example, arithmetic operations
+The following grammar is definitely coarse for any x86-64 ISA.
+It's just a good start to make instruction selection work. For example, arithmetic operations
 can access memory, but here we show rules that operate on registers only.
 TODO: some temporaries will turn out to be memory locations. They cannot be regarded as registers.
 Numbers are appended to each nonterminal on the RHS in order to see how to perform
-the code generation. Code to be emitted is shown after the '::' token.
+the code generation. Code to be emitted is shown after the first '::' token.
+An instruction "cost" is shown after the second '::' token. This is taken from
+https://www.agner.org/optimize/instruction_tables.pdf.
+On modern processors, the execution time of a code sequence is not easily predictable from the individual instructions, 
+due to interactions between instructions and their shared hardware resources.
+The costs used in this algorithm are taken from the 
+reciprocal throughput column of the AMD Zen 2 architecture.
+
 Calls and jumps to labels are always near.
 
 %term const, temp
@@ -19,57 +26,58 @@ Calls and jumps to labels are always near.
 
 %%
 
-goal : goal stmt
-goal : stmt
+goal : goal stmt :: :: 0
+goal : stmt :: :: 0
 
-a1 :  BinOpExp(reg1, reg2, plus)
-a2 :  BinOpExp(reg, ConstExp(const), plus)
-a2 :  BinOpExp(ConstExp(const), reg), plus)
-a3 :  ConstExp(const)
-a4 :  ConstExp(0)
+a1 :  BinOpExp(reg1, reg2, plus) :: :: 0
+a2 :  BinOpExp(reg, ConstExp(const), plus)  :: :: 0
+a2 :  BinOpExp(ConstExp(const), reg), plus) :: :: 0
+a3 :  ConstExp(const) :: :: 0
+a4 :  ConstExp(0) :: :: 0
 
-reg_list : reg | epsilon
+reg_list : reg | epsilon :: :: 0
 
-lab : NameExp(label)
+lab : NameExp(label) :: :: 0
 
-reg : ConstExp(const) :: move new_reg, const
-reg : NameExp(label)  :: move new_reg, label
-reg : TempExp(temp)
+reg : ConstExp(const) :: move new_reg, const :: 0.25
+reg : NameExp(label)  :: move new_reg, label :: 0.25
+reg : TempExp(temp) :: :: 0
 
-reg : BinOpExp(reg1, reg2, plus)  :: move new_reg, reg1; add new_reg, reg2	
-reg : BinOpExp(reg1, reg2, minus) :: move new_reg, reg1; sub new_reg, reg2
-reg : BinOpExp(reg1, reg2, mul)   :: move rax, reg1; imul reg2; move new_reg, rax
-reg : BinOpExp(reg1, reg2, div)   :: move rax, reg1; idiv reg2; move new_reg, rax
+reg : BinOpExp(reg1, reg2, plus)  :: move new_reg, reg1; add new_reg, reg2 :: 0.25 + 0.3 = 0.55 
+reg : BinOpExp(reg1, reg2, minus) :: move new_reg, reg1; sub new_reg, reg2 :: 0.25 + 0.3 = 0.55
+reg : BinOpExp(reg1, reg2, mul)   :: move rax, reg1; imul reg2; move new_reg, rax :: 0.25 + 1 + 0.25 = 1.5 
+reg : BinOpExp(reg1, reg2, div)   :: move rax, reg1; idiv reg2; move new_reg, rax :: 0.25 + 13 + 0.25 = 13.5 
 
-reg : MemExp(reg1) 	:: move new_reg, [reg1]
-reg : MemExp(a1)   	:: move new_reg, [a1.reg1 + a1.reg2]
-reg : MemExp(a2)  	:: move new_reg, [a2.reg + const]
-reg : MemExp(a3)   	:: move new_reg, [const]
+reg : MemExp(reg1) 	:: move new_reg, [reg1] :: 0.5
+reg : MemExp(a1)   	:: move new_reg, [a1.reg1 + a1.reg2] :: 0.5
+reg : MemExp(a2)  	:: move new_reg, [a2.reg + const] :: 0.5
+reg : MemExp(a3)   	:: move new_reg, [const] :: 0.5
 
-reg : CallExp(reg1, reg_list) 	:: call reg1;             move new_reg, eax
-reg : CallExp(lab,  reg_list)  	:: call rel32 lab.label;  move new_reg, eax
+reg : CallExp(reg1, reg_list) 	:: call reg1;             move new_reg, eax :: 2 + 0.25 = 2.25
+reg : CallExp(lab,  reg_list)  	:: call rel32 lab.label;  move new_reg, eax :: 2 + 0.25 = 2.25
 
-stmt : MoveStmt(reg1, reg2) :: move reg1, reg2
-stmt : MoveStmt(reg1, a1) 	:: move reg1, [a1.reg1 + a1.reg2]
-stmt : MoveStmt(a1, reg2) 	:: move QWORD PTR [a1.reg1 + a1.reg2], reg2
-stmt : MoveStmt(reg1, a2) 	:: move reg1, [a2.reg1 + a2.const]
-stmt : MoveStmt(a2, reg2) 	:: move QWORD PTR [a2.reg1 + a2.const], reg2
-stmt : MoveStmt(reg1, a3) 	:: move reg1, [a3.const]
-stmt : MoveStmt(reg1, a4) 	:: xor reg1, reg1
+stmt : MoveStmt(reg1, reg2) :: move reg1, reg2 :: 0.25
+stmt : MoveStmt(reg1, a1) 	:: move reg1, [a1.reg1 + a1.reg2] :: 0.5
+stmt : MoveStmt(reg1, a2) 	:: move reg1, [a2.reg1 + a2.const] :: 0.5
+stmt : MoveStmt(reg1, a3) 	:: move reg1, [a3.const] :: 0.5
 
-stmt : ExpStmt(CallExp(lab, reg_list)) :: call rel32 lab.label
-stmt : ExpStmt(CallExp(reg, reg_list)) :: call reg
-stmt : ExpStmt(reg)
+stmt : MoveStmt(a1, reg2) 	:: move QWORD PTR [a1.reg1 + a1.reg2], reg2  :: 1
+stmt : MoveStmt(a2, reg2) 	:: move QWORD PTR [a2.reg1 + a2.const], reg2 :: 1
+stmt : MoveStmt(reg1, a4) 	:: xor reg1, reg1 :: 0.3
 
-stmt : JumpStmt(reg) 		:: jmp reg
-stmt : JumpStmt(lab) 	  :: jmp rel32 lab.label
+stmt : ExpStmt(CallExp(lab, reg_list)) :: call rel32 lab.label :: 2
+stmt : ExpStmt(CallExp(reg, reg_list)) :: call reg :: 2
+stmt : ExpStmt(reg) :: :: 0
 
-stmt : LabelStmt(label) :: label:
+stmt : JumpStmt(reg) 		:: jmp reg :: 2
+stmt : JumpStmt(lab) 	  :: jmp rel32 lab.label :: 2
 
-stmt : CJumpStmt(eq, reg1, reg2, tlab, flab) :: cmp reg1, reg2; je tlab
-stmt : CJumpStmt(ne, reg1, reg2, tlab, flab) :: cmp reg1, reg2; jne tlab
-stmt : CJumpStmt(lt, reg1, reg2, tlab, flab) :: cmp reg1, reg2; jl tlab
-stmt : CJumpStmt(gt, reg1, reg2, tlab, flab) :: cmp reg1, reg2; jg tlab
-stmt : CJumpStmt(le, reg1, reg2, tlab, flab) :: cmp reg1, reg2; jle tlab
-stmt : CJumpStmt(ge, reg1, reg2, tlab, flab) :: cmp reg1, reg2; jge tlab
+stmt : LabelStmt(label) :: label: :: 0
+
+stmt : CJumpStmt(eq, reg1, reg2, tlab, flab) :: cmp reg1, reg2; je tlab  :: 0.25 + 0.5 = 0.75
+stmt : CJumpStmt(ne, reg1, reg2, tlab, flab) :: cmp reg1, reg2; jne tlab :: 0.25 + 0.5 = 0.75
+stmt : CJumpStmt(lt, reg1, reg2, tlab, flab) :: cmp reg1, reg2; jl tlab  :: 0.25 + 0.5 = 0.75
+stmt : CJumpStmt(gt, reg1, reg2, tlab, flab) :: cmp reg1, reg2; jg tlab  :: 0.25 + 0.5 = 0.75
+stmt : CJumpStmt(le, reg1, reg2, tlab, flab) :: cmp reg1, reg2; jle tlab :: 0.25 + 0.5 = 0.75
+stmt : CJumpStmt(ge, reg1, reg2, tlab, flab) :: cmp reg1, reg2; jge tlab :: 0.25 + 0.5 = 0.75
 */
